c16caf10a4481b8ddd92d028d07f424b0daaa742
Sandro Doro <sandro.doro@gmail.com>
2014-06-04 15:45:01 +0200

    Fixed q1, q2 and q4 queries.

--- views.py
+++ views.py
@@ -2,30 +2,44 @@
 from django.http import HttpResponse
 from pra.models import *
 
+# q1:  La lista delle automobili immatricolate in un certo anno
 def q1(request):
-    elenco = ""
-    for v in veicolo.objects.all():
-        elenco += "%s<br>" % (v.targa)
-    return HttpResponse(elenco)
-
-def q2(request):
-    elenco = ""
+    elenco = "Lista delle automobili immatricolate nel 2013<br>"
     vs = veicolo.objects.all()
     vs = vs.filter(data_im__range=["2013-01-01", "2013-12-31"])
-    vs = vs.filter(data_omo__isnull=False)   # non vogliamo i prototipi
     for v in vs:
         elenco += "%s<br>" % (v.targa)
     return HttpResponse(elenco)
 
+# q2: Per ogni modello, il numero di veicoli immatricolati tra il 2010 e il
+#     2014, solo se superiore a uno.
+from django.db.models import Count
+def q2(request):
+    elenco = ""
+    vs = veicolo.objects.all()
+    vs = vs.filter(data_im__range=["2010-01-01", "2013-12-31"])
+    vs = vs.filter(data_omo__isnull=False)   # non vogliamo i prototipi
+    vs = vs.values('nome').annotate(cnt=Count('nome'))
+    for v in vs:
+        if v['cnt'] > 1:
+           elenco += "%s - %s<br>" % (v['nome'], v['cnt'])
+    if elenco == "":
+        elenco = "Nessun risultato"
+    return HttpResponse(elenco)
+
+# q3. L'elenco delle patenti in scadenza in ordine di prefettura
 import datetime
 def q3(request):
     elenco = ""
     ps = patente.objects.all()
     ps = ps.filter(data_scadenza__lt=datetime.date(2014, 6, 23))
+    ps = ps.order_by('prefettura')
     for p in ps:
-        elenco += "%s<br>" % (p.cod_pat)
+        elenco += "%s - %s<br>" % (p.cod_pat, p.prefettura)
     return HttpResponse(elenco)
 
+# q4: Numero massimo di patentati ultrasessantenni nelle province di Venezia,
+#     Treviso, Padova
 from django.db import connection
 def q4(request):
     elenco = "Numero max: "
@@ -45,7 +59,6 @@ FROM
     cursor = connection.cursor()
     cursor.execute(query)
     elenco += "%s<br>" % cursor.fetchall()[0]
-    #elenco += "%s<br>" % cursor.fetchall()
 
     return HttpResponse(elenco)
 
